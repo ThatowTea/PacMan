b8ccf773374f2fa52e7cd1a312d25c07f60bf765
ThatowTea <thatonkitseng99@gmail.com>
2024-11-05 17:10:47 +0200

    PacMan

---
+++ Assets/Scripts/ShowScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour {
+
+	// Use this for initialization
+
+	int score = 0;
+	public Text txtScore;
+	void Start () {
+		score = PlayerPrefs.GetInt ("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txtScore.text = "Your score: " + score;
+	}
+}
---
+++ Assets/Scripts/scorer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scorer : MonoBehaviour {
+
+	// Use this for initialization
+	private int score;
+	public Text txtScore;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txtScore.text = "Score: " + score;
+	}
+	public void AddScore(int toAdd)
+	{
+		score += toAdd;
+		PlayerPrefs.SetInt ("Score", score);
+		if(score == 4)
+		{
+			SceneManager.LoadScene (1);
+		}
+	}
+
+}
